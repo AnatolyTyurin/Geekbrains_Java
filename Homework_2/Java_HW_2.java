293a22f5c99fc751e108e8ea9566c1b94a106892
Анатолий Тюрин <tyurinanatoly@gmail.com>
2023-04-05 17:32:54 +0300

    HW2 code optimization

--- Homework_2/Java_HW_2.java
+++ Homework_2/Java_HW_2.java
@@ -1,18 +1,33 @@
 class Java_HW_2 {
     public static void main(String[] args) {
-        System.out.println(checkSumSign(2, 20));
+        System.out.println(between10And20(-2, 11));
+        System.out.println(between10And20(3, 8));
+        System.out.println(between10And20(15, 6));
+        
+        System.out.println(checkNumber(-1)? "Number is positive" : "Number is negative");
         System.out.println(checkNumber(0)? "Number is positive" : "Number is negative");
+        System.out.println(checkNumber(1)? "Number is positive" : "Number is negative");
+
+
         System.out.println(checkNumberBoolean(-1));
+        System.out.println(checkNumberBoolean(0));
+        System.out.println(checkNumberBoolean(1));
+        
         lineOut("__Java is great!___", 2);
-        System.out.println(leapYear(2024));
+        
+        System.out.println(leapYear(2000));
+        System.out.println(leapYear(2003));
+        System.out.println(leapYear(2004));
+        System.out.println(leapYear(2100));
+        
+        System.out.println(leapYearOptimize(2000));
+        System.out.println(leapYearOptimize(2003));
+        System.out.println(leapYearOptimize(2004));
+        System.out.println(leapYearOptimize(2100));
     }
 
-    static boolean checkSumSign(int a, int b) {
-        if (a + b <= 20 && a + b >= 10) {
-        return true;
-        } else {
-            return false;
-        }
+    static boolean between10And20(int a, int b) {
+        return a + b >= 10 && a + b <= 20;
     }
 
     static boolean checkNumber(int a) {
@@ -20,11 +35,7 @@ class Java_HW_2 {
     }
 
     static boolean checkNumberBoolean(int a) {
-        if (a >= 0) {
-            return false;
-        } else {
-            return true;
-        }
+        return a < 0;
     }
 
     static void lineOut(String a, int b) {
@@ -43,5 +54,9 @@ class Java_HW_2 {
         } else {
             return false;
         }
+    }
+
+    static boolean leapYearOptimize(int year) {
+        return (year % 4 == 0 && year % 100 !=0) || year % 400 == 0;
     }
 }
